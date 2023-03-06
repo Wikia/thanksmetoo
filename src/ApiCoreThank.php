e4642dd7c6af51f90d92dcb954a913ae5baf6c13
mikolajmeller <mmeller@wikia-inc.com>
2023-03-06 10:46:50 +0100

    PLATFORM-7893 upgrade ThanksMeToo: upgrade API classes

--- src/ApiCoreThank.php
+++ src/ApiCoreThank.php
@@ -1,94 +1,86 @@
 <?php
-/**
- * API module to send thanks notifications for revisions and log entries.
- *
- * @ingroup API
- * @ingroup Extensions
- */
 
 namespace ThanksMeToo;
 
-use ActorMigration;
 use ApiBase;
 use DatabaseLogEntry;
-use LogEntry;
 use ManualLogEntry;
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionLookup;
 use MediaWiki\Revision\RevisionRecord;
-use Reverb\Notification\NotificationBroadcast;
+use MediaWiki\Revision\RevisionStore;
+use MediaWiki\User\UserFactory;
+use Reverb\Notification\NotificationBroadcastFactory;
 use Title;
 use User;
 use Wikimedia\ParamValidator\ParamValidator;
 use Wikimedia\ParamValidator\TypeDef\NumericDef;
+use Wikimedia\Rdbms\ILoadBalancer;
 
 class ApiCoreThank extends ApiBase {
-	/**
-	 * Perform the API request.
-	 */
+	public function __construct(
+		$query,
+		$moduleName,
+		private UserFactory $userFactory,
+		private ILoadBalancer $loadBalancer,
+		private NotificationBroadcastFactory $notificationBroadcastFactory,
+		private RevisionStore $revisionStore,
+		private RevisionLookup $revisionLookup
+	) {
+		parent::__construct( $query, $moduleName );
+	}
+
+	/** @inheritDoc */
 	public function execute() {
-		// Initial setup.
 		$user = $this->getUser();
 		$this->dieOnBadUser( $user );
 		$params = $this->extractRequestParams();
-		$revcreation = false;
-
 		$this->requireOnlyOneParameter( $params, 'rev', 'log' );
 
-		// Extract type and ID from the parameters.
-		if ( isset( $params['rev'] ) && !isset( $params['log'] ) ) {
-			$type = 'rev';
-			$id = $params['rev'];
-		} elseif ( !isset( $params['rev'] ) && isset( $params['log'] ) ) {
-			$type = 'log';
-			$id = $params['log'];
-		} else {
-			$this->dieWithError( 'thanks-error-api-params', 'thanks-error-api-params' );
-		}
-
-		// Determine thanks parameters.
-		if ( $type === 'log' ) {
-			$logEntry = $this->getLogEntryFromId( $id );
-			// If there's an associated revision, thank for that instead.
-			if ( $logEntry->getAssociatedRevId() ) {
-				$type = 'rev';
-				$id = $logEntry->getAssociatedRevId();
-			} else {
-				$excerpt = '';
-				$title = $logEntry->getTarget();
-				$recipient = $this->getUserFromLog( $logEntry );
-				$recipientUsername = $recipient->getName();
-			}
-		}
-		if ( $type === 'rev' ) {
-			$revision = $this->getRevisionFromId( $id );
-			$title = $this->getTitleFromRevision( $revision );
-			$recipient = $this->getUserFromRevision( $revision );
-
-			// If there is no parent revid of this revision, it's a page creation.
-			$previousRevision = MediaWikiServices::getInstance()
-				->getRevisionLookup()->getPreviousRevision( $revision );
-
-			if ( !$previousRevision ) {
-				$revcreation = true;
-			}
-		}
+		[ $type, $id, $recipient, $title, $revcreation ] = $this->calculateParams( $params );
 
 		// Send thanks.
 		if ( $this->userAlreadySentThanks( $user, $type, $id ) ) {
-			$this->markResultSuccess( $recipientUsername->getName() );
+			$this->markResultSuccess( $recipient->getName() );
 		} else {
 			$this->dieOnBadRecipient( $user, $recipient );
 			$this->sendThanks(
 				$user,
 				$type,
 				$id,
-				'',
 				$recipient,
-				$this->getSourceFromParams( $params ),
 				$title,
 				$revcreation
 			);
 		}
+	}
+
+	/** @return array of [ string $type, int $id, User $recipient, Title $title, bool $revcreation ] */
+	private function calculateParams( array $params ): array {
+		$type = isset( $params['log'] ) ? 'log' : 'rev';
+		$id = isset( $params['log'] ) ? (int)$params['log'] : (int)$params['rev'];
+
+		// Determine thanks parameters.
+		if ( $type === 'log' ) {
+			$logEntry = $this->getLogEntryFromId( $id );
+			// If there's an associated revision, thank for that instead.
+			if ( !$logEntry->getAssociatedRevId() ) {
+				$recipient = $this->userFactory->newFromUserIdentity( $logEntry->getPerformerIdentity() );
+				return [ $type, $id, $recipient, $logEntry->getTarget(), false ];
+			}
+
+			// Use associated revision from this log
+			$type = 'rev';
+			$id = $logEntry->getAssociatedRevId();
+		}
+
+		$revision = $this->getRevisionFromId( $id );
+		$title = $this->getTitleFromRevision( $revision );
+		$recipient = $this->getUserFromRevision( $revision );
+
+		// If there is no parent revid of this revision, it's a page creation.
+		$revcreation = !$this->revisionLookup->getPreviousRevision( $revision );
+
+		return [ $type, $id, $recipient, $title, $revcreation ];
 	}
 
 	/**
@@ -99,7 +91,7 @@ class ApiCoreThank extends ApiBase {
 	 * @param int $id The revision or log ID.
 	 * @return bool
 	 */
-	private function userAlreadySentThanks( User $user, $type, $id ) {
+	private function userAlreadySentThanks( User $user, string $type, int $id ): bool {
 		if ( $type === 'rev' ) {
 			// For b/c with old-style keys
 			$type = '';
@@ -107,27 +99,22 @@ class ApiCoreThank extends ApiBase {
 		return (bool)$user->getRequest()->getSessionData( "thanks-thanked-$type$id" );
 	}
 
-	private function getRevisionFromId( $revId ) {
-		$revision = MediaWikiServices::getInstance()
-			->getRevisionStore()
-			->getRevisionById( $revId );
+	private function getRevisionFromId( int $revId ): RevisionRecord {
+		$revision = $this->revisionStore->getRevisionById( $revId );
 		// Revision ID 1 means an invalid argument was passed in.
 		if ( !$revision || $revision->getId() === 1 ) {
 			$this->dieWithError( 'thanks-error-invalidrevision', 'invalidrevision' );
-		} elseif ( $revision->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
+		}
+
+		if ( $revision->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 			$this->dieWithError( 'thanks-error-revdeleted', 'revdeleted' );
 		}
+
 		return $revision;
 	}
 
-	/**
-	 * Get the log entry from the ID.
-	 *
-	 * @param int $logId The log entry ID.
-	 * @return DatabaseLogEntry
-	 */
-	private function getLogEntryFromId( $logId ) {
-		$logEntry = DatabaseLogEntry::newFromId( $logId, wfGetDB( DB_REPLICA ) );
+	private function getLogEntryFromId( int $logId ): DatabaseLogEntry {
+		$logEntry = DatabaseLogEntry::newFromId( $logId, $this->loadBalancer->getConnection( DB_REPLICA ) );
 
 		if ( !$logEntry ) {
 			$this->dieWithError( 'thanks-error-invalid-log-id', 'thanks-error-invalid-log-id' );
@@ -148,44 +135,20 @@ class ApiCoreThank extends ApiBase {
 		return $logEntry;
 	}
 
-	private function getTitleFromRevision( RevisionRecord $revision ) {
-		$title = Title::newFromID( $revision->getPage()->getId() );
-		if ( !$title instanceof Title ) {
+	private function getTitleFromRevision( RevisionRecord $revision ): Title {
+		$title = Title::castFromPageIdentity( $revision->getPage() );
+		if ( !$title ) {
 			$this->dieWithError( 'thanks-error-notitle', 'notitle' );
 		}
 		return $title;
 	}
 
-	/**
-	 * Set the source of the thanks, e.g. 'diff' or 'history'
-	 *
-	 * @param string[] $params Incoming API parameters, with a 'source' key.
-	 * @return string The source, or 'undefined' if not provided.
-	 */
-	private function getSourceFromParams( $params ) {
-		if ( $params['source'] ) {
-			return trim( $params['source'] );
-		} else {
-			return 'undefined';
-		}
-	}
-
-	private function getUserFromRevision( RevisionRecord $revision ) {
+	private function getUserFromRevision( RevisionRecord $revision ): User {
 		$recipient = $revision->getUser();
 		if ( !$recipient ) {
 			$this->dieWithError( 'thanks-error-invalidrecipient', 'invalidrecipient' );
 		}
-		return MediaWikiServices::getInstance()
-			->getUserFactory()
-			->newFromId( $recipient->getId() );
-	}
-
-	private function getUserFromLog( LogEntry $logEntry ) {
-		$recipient = $logEntry->getPerformer();
-		if ( !$recipient ) {
-			$this->dieWithError( 'thanks-error-invalidrecipient', 'invalidrecipient' );
-		}
-		return $recipient;
+		return $this->userFactory->newFromUserIdentity( $recipient );
 	}
 
 	/**
@@ -194,20 +157,15 @@ class ApiCoreThank extends ApiBase {
 	 * @param User $agent The thanks-sending user.
 	 * @param string $type The thanks type ('rev' or 'log').
 	 * @param int $id The log or revision ID.
-	 * @param string $excerpt The excerpt to display as the thanks notification. This will only
-	 *                             be used if it is not possible to retrieve the relevant excerpt at
-	 *                             the time the notification is displayed (in order to account for
-	 *                             changing visibility in the meantime).
 	 * @param User $recipient The recipient of the thanks.
-	 * @param string $source Where the thanks was given.
 	 * @param Title $title The title of the page for which thanks is given.
 	 * @param bool $revcreation True if the linked revision is a page creation.
 	 *
 	 * @return void
 	 */
 	private function sendThanks(
-		User $agent, $type, $id, $excerpt, User $recipient, $source, Title $title, $revcreation
-	) {
+		User $agent, string $type, int $id, User $recipient, Title $title, bool $revcreation
+	): void {
 		$uniqueId = $type . '-' . $id;
 		// Do one last check to make sure we haven't sent Thanks before
 		if ( $this->haveAlreadyThanked( $agent, $uniqueId ) ) {
@@ -218,41 +176,20 @@ class ApiCoreThank extends ApiBase {
 
 		$agentUserTitle = Title::makeTitle( NS_USER_PROFILE, $agent->getName() );
 		$recipientUserTitle = Title::makeTitle( NS_USER_PROFILE, $recipient->getName() );
-		$broadcast = NotificationBroadcast::newSingle(
+		$broadcast = $this->notificationBroadcastFactory->newSingle(
 			'user-interest-thanks-' . ( $revcreation ? 'creation' : 'edit' ),
 			$agent,
 			$recipient,
 			[
 				'url' => $title->getFullUrl(),
 				'message' => [
-					[
-						'user_note',
-						''
-					],
-					[
-						1,
-						$agentUserTitle->getFullUrl()
-					],
-					[
-						2,
-						$agent->getName()
-					],
-					[
-						3,
-						$recipientUserTitle->getFullURL()
-					],
-					[
-						4,
-						$recipient->getName()
-					],
-					[
-						5,
-						$title->getFullURL()
-					],
-					[
-						6,
-						$title->getFullText()
-					]
+					[ 'user_note', '' ],
+					[ 1, $agentUserTitle->getFullUrl() ],
+					[ 2, $agent->getName() ],
+					[ 3, $recipientUserTitle->getFullURL() ],
+					[ 4, $recipient->getName() ],
+					[ 5, $title->getFullURL() ],
+					[ 6, $title->getFullText() ]
 				]
 			]
 		);
@@ -267,6 +204,7 @@ class ApiCoreThank extends ApiBase {
 		$this->logThanks( $agent, $recipient, $uniqueId );
 	}
 
+	/** @inheritDoc */
 	public function getAllowedParams() {
 		return [
 			'rev' => [
@@ -291,14 +229,20 @@ class ApiCoreThank extends ApiBase {
 	}
 
 	private function dieOnBadUser( User $user ) {
-		$userBlock = $user->getBlock();
 		if ( $user->isAnon() ) {
 			$this->dieWithError( 'thanks-error-notloggedin', 'notloggedin' );
-		} elseif ( $user->pingLimiter( 'thanks-notification' ) ) {
+		}
+
+		if ( $user->pingLimiter( 'thanks-notification' ) ) {
 			$this->dieWithError( [ 'thanks-error-ratelimited', $user->getName() ], 'ratelimited' );
-		} elseif ( $userBlock && $userBlock->isSitewide() ) {
+		}
+
+		$userBlock = $user->getBlock();
+		if ( $userBlock && $userBlock->isSitewide() ) {
 			$this->dieBlocked( $userBlock );
-		} elseif ( $user->isBlockedGlobally() ) {
+		}
+
+		if ( $user->isBlockedGlobally() ) {
 			$this->dieBlocked( $user->getGlobalBlock() );
 		}
 	}
@@ -306,39 +250,35 @@ class ApiCoreThank extends ApiBase {
 	private function dieOnBadRecipient( User $user, User $recipient ) {
 		if ( $user->getId() === $recipient->getId() ) {
 			$this->dieWithError( 'thanks-error-invalidrecipient-self', 'invalidrecipient' );
-		} elseif ( !$this->getConfig()->get( 'ThanksSendToBots' ) && $recipient->isBot() ) {
+		}
+
+		if ( !$this->getConfig()->get( 'ThanksSendToBots' ) && $recipient->isBot() ) {
 			$this->dieWithError( 'thanks-error-invalidrecipient-bot', 'invalidrecipient' );
 		}
 	}
 
-	private function markResultSuccess( $recipientName ) {
+	private function markResultSuccess( string $recipientName ): void {
 		$this->getResult()->addValue( null, 'result', [
 			'success' => 1,
 			'recipient' => $recipientName,
 		] );
 	}
 
-	/**
-	 * This checks the log_search data.
-	 *
-	 * @param User $thanker The user sending the thanks.
-	 * @param string $uniqueId The identifier for the thanks.
-	 * @return bool Whether thanks has already been sent
-	 */
-	private function haveAlreadyThanked( User $thanker, $uniqueId ) {
-		$dbw = wfGetDB( DB_PRIMARY );
-		$logWhere = ActorMigration::newMigration()->getWhere( $dbw, 'log_user', $thanker );
-		return (bool)$dbw->selectRow(
-			[ 'log_search', 'logging' ] + $logWhere['tables'],
+	private function haveAlreadyThanked( User $thanker, string $uniqueId ): bool {
+		return (bool)$this->loadBalancer->getConnection( DB_PRIMARY )->selectRow(
+			[ 'log_search', 'logging', 'actor' ],
 			[ 'ls_value' ],
 			[
-				$logWhere['conds'],
+				'actor_user' => $thanker->getId(),
 				'ls_field' => 'thankid',
 				'ls_value' => $uniqueId,
 			],
 			__METHOD__,
 			[],
-			[ 'logging' => [ 'INNER JOIN', 'ls_log_id=log_id' ] ] + $logWhere['joins']
+			[
+				'logging' => [ 'INNER JOIN', 'ls_log_id=log_id' ],
+				'actor' => [ 'JOIN', 'actor_id=log_actor']
+			]
 		);
 	}
 
@@ -348,7 +288,7 @@ class ApiCoreThank extends ApiBase {
 	 * @param string $uniqueId A unique Id to identify the event being thanked for, to use
 	 *                          when checking for duplicate thanks
 	 */
-	private function logThanks( User $user, User $recipient, $uniqueId ) {
+	private function logThanks( User $user, User $recipient, string $uniqueId ): void {
 		if ( !$this->getConfig()->get( 'ThanksLogging' ) ) {
 			return;
 		}
@@ -361,25 +301,25 @@ class ApiCoreThank extends ApiBase {
 		$logEntry->publish( $logId, 'udp' );
 	}
 
+	/** @inheritDoc */
 	public function needsToken() {
 		return 'csrf';
 	}
 
+	/** @inheritDoc */
 	public function isWriteMode() {
 		// Writes to the Echo database and sometimes log tables.
 		return true;
 	}
 
+	/** @inheritDoc */
 	public function getHelpUrls() {
 		return [
 			'https://www.mediawiki.org/wiki/Extension:Thanks#API_Documentation',
 		];
 	}
 
-	/**
-	 * @see    ApiBase::getExamplesMessages()
-	 * @return array
-	 */
+	/** @inheritDoc */
 	protected function getExamplesMessages() {
 		return [
 			'action=thank&revid=456&source=diff&token=123ABC'
